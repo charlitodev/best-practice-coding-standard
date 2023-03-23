1adb51a081fba11b1d8ccbbb1fbf5077ed4079a2
charlitodev <charlito@qualitytrade.com>
2023-03-23 09:17:38 -0700

    update

--- src/configs/mediaData.js
+++ src/configs/mediaData.js
@@ -1,66 +1,66 @@
 export const mediaData = [
   {
     id: 1,
-    title: "For those who want to relax",
-    youtuber: "Morning Route",
+    title: "The Most Attractive Thumbnail",
+    youtuber: "Kids Diana Show",
     views: "246K views",
     thumbnail:
       "https://marketplace.canva.com/EAEqfS4X0Xw/1/0/1600w/canva-most-attractive-youtube-thumbnail-wK95f3XNRaM.jpg",
   },
   {
     id: 2,
-    title: "For those who want to relax",
-    youtuber: "Morning Route",
+    title: "How to Create Amazing Resume",
+    youtuber: "Juega German",
     views: "246K views",
     thumbnail:
-      "https://marketplace.canva.com/EAEqfS4X0Xw/1/0/1600w/canva-most-attractive-youtube-thumbnail-wK95f3XNRaM.jpg",
+      "https://static-cse.canva.com/blob/1028383/1600w-y9mFJemyO0E.jpg",
   },
   {
     id: 3,
     title: "For those who want to relax",
-    youtuber: "Morning Route",
+    youtuber: "Vlad and Niki",
     views: "246K views",
     thumbnail:
-      "https://marketplace.canva.com/EAEqfS4X0Xw/1/0/1600w/canva-most-attractive-youtube-thumbnail-wK95f3XNRaM.jpg",
+      "https://fiverr-res.cloudinary.com/images/q_auto,f_auto/gigs/118596058/original/6253401eb43dc83fa4d5c41105f6f3c096a28e41/design-catchy-youtube-thumbnail.jpg",
   },
   {
     id: 4,
-    title: "For those who want to relax",
-    youtuber: "Morning Route",
+    title: "PewDiePie Laugh or Lose",
+    youtuber: "PewDiePie",
     views: "246K views",
     thumbnail:
-      "https://marketplace.canva.com/EAEqfS4X0Xw/1/0/1600w/canva-most-attractive-youtube-thumbnail-wK95f3XNRaM.jpg",
+      "https://www.wyzowl.com/wp-content/uploads/2021/12/img_61b9cdbdd7c04.png",
   },
   {
     id: 5,
-    title: "For those who want to relax",
-    youtuber: "Morning Route",
+    title: "Make Money in a Fast Way",
+    youtuber: "Hackerman",
     views: "246K views",
     thumbnail:
-      "https://marketplace.canva.com/EAEqfS4X0Xw/1/0/1600w/canva-most-attractive-youtube-thumbnail-wK95f3XNRaM.jpg",
+      "https://static-cse.canva.com/blob/1028372/1600w-e6QKC2GbjAQ.jpg",
   },
   {
     id: 6,
-    title: "For those who want to relax",
+    title: "Boost your Finances",
     youtuber: "Morning Route",
     views: "246K views",
     thumbnail:
-      "https://marketplace.canva.com/EAEqfS4X0Xw/1/0/1600w/canva-most-attractive-youtube-thumbnail-wK95f3XNRaM.jpg",
+      "https://i.easil.com/wp-content/uploads/20210901140229/Block-Heading-Falling-Cash-Template-youtube-thumbnail.jpg",
   },
   {
     id: 7,
-    title: "For those who want to relax",
-    youtuber: "Morning Route",
+    title: "$10,000 LAST TO LEAVE",
+    youtuber: "Mr Beast",
     views: "246K views",
     thumbnail:
-      "https://marketplace.canva.com/EAEqfS4X0Xw/1/0/1600w/canva-most-attractive-youtube-thumbnail-wK95f3XNRaM.jpg",
+      "https://i.etsystatic.com/38667211/r/il/189b28/4372660609/il_fullxfull.4372660609_d0m1.jpg",
   },
   {
     id: 8,
-    title: "For those who want to relax",
-    youtuber: "Morning Route",
+    title: "Dropshipping tips and tricks",
+    youtuber: "T-Series",
     views: "246K views",
     thumbnail:
-      "https://marketplace.canva.com/EAEqfS4X0Xw/1/0/1600w/canva-most-attractive-youtube-thumbnail-wK95f3XNRaM.jpg",
+      "https://fiverr-res.cloudinary.com/images/t_main1,q_auto,f_auto,q_auto,f_auto/gigs/207120071/original/cc48fb7a50aa694832f50f0a2c33e16cd9dfbe0a/design-an-amazing-youtube-thumbnail-in-12-hours.jpg",
   },
 ];
